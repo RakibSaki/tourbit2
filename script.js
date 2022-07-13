44d57aa3a3522c1466e3ddd9051dcb892b13db6d
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-07-13 22:15:26 +0600

    little much

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let th = 0
-let r = 100
+let θ = 0
+let r = 1.471e11
+let v = 30290
+
+let µ = 1.98847e30 * 6.6743e-11
 
 draw = () => {
-    document.querySelector('.planet').style.transform = `rotate(${th}rad) translate(${r}px)`
-    th += 0.01
+    document.querySelector('.planet').style.transform = `rotate(${θ}rad) translate(${r}px)`
+    θ += 0.01
 }
